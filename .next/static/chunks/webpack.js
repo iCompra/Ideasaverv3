77e64a6c876008c0850811c5859c327f6734b535
iCompra <iCompra.contacto@gmail.com>
2025-07-13 13:34:07 -0400

    Create .env file with Supabase configuration

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c450aade1c8a3814"; }
+/******/ 		__webpack_require__.h = function() { return "016e341dbb8ecbbb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
